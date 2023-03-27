f4c4f0e1d1474e2a7b14a8f14742d1dec1a107e3
Ray Kazuya Murphy <85945564+Rayezor@users.noreply.github.com>
2023-03-27 18:02:02 +0100

    Add project files.

---
+++ Stockify/Models/Stock.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stockify.Models
+{
+    public class Stock
+    {
+        public enum Categories
+        {
+            Energy,
+            Materials,
+            Industrials,
+            Discretionary,
+            ConsumerStaples,
+            Healthcare,
+            Financials,
+            Technology,
+            Communication,
+            Utilities,
+            RealEstate
+        }
+        [Key]
+        [Required]
+        public string Symbol { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public Categories Category { get; set; }
+        public DateTime IPODate { get; set; }
+        public float Price { get; set; }
+    }
+}
