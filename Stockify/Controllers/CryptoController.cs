bc9fb3f9ca447591346d38b36a88b53d2368df41
JakeW <X00170642@myTUDublin.ie>
2023-04-02 14:54:27 +0100

    Added Crypto Controller and View. Added Crypto to Nav bar in Layout.cshtml on the Index Page.

---
+++ Stockify/Controllers/CryptoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stockify.Models;
+
+namespace Stockify.Controllers
+{
+    public class CryptoController : Controller
+    {
+
+        private static List<Crypto> cryptolist = new List<Crypto>()
+        {
+            new Crypto{Name="Bitcoin", Prefix="BTC", MarketCap=489.6, Price=29485, DateCreated=new DateOnly(2004, 1, 7), CreatedBy="Trump International"},
+            new Crypto{Name="Bitcoin", Prefix="BTC", MarketCap=489.6, Price=29485, DateCreated=new DateOnly(2004, 1, 7), CreatedBy="Trump International"},
+            new Crypto{Name="Bitcoin", Prefix="BTC", MarketCap=489.6, Price=29485, DateCreated=new DateOnly(2004, 1, 7), CreatedBy="Trump International"},
+            new Crypto{Name="Bitcoin", Prefix="BTC", MarketCap=489.6, Price=29485, DateCreated=new DateOnly(2004, 1, 7), CreatedBy="Trump International"},
+            new Crypto{Name="Bitcoin", Prefix="BTC", MarketCap=489.6, Price=29485, DateCreated=new DateOnly(2004, 1, 7), CreatedBy="Trump International"},
+            new Crypto{Name="Bitcoin", Prefix="BTC", MarketCap=489.6, Price=29485, DateCreated=new DateOnly(2004, 1, 7), CreatedBy="Trump International"},
+            new Crypto{Name="Bitcoin", Prefix="BTC", MarketCap=489.6, Price=29485, DateCreated=new DateOnly(2004, 1, 7), CreatedBy="Trump International"},
+
+        };
+
+        // GET: CryptoController
+        public ActionResult Crypto()
+        {
+            return View();
+        }
+
+      
+    }
+}
--- Stockify/Models/Crypto.cs
+++ Stockify/Models/Crypto.cs
@@ -1,12 +1,31 @@
-﻿using System.Net.Security;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Security;
 
 namespace Stockify.Models
 {
-    public class test
+    public class Crypto
     {
-        private string Name;
-        private string Descrition;
-        public int Value;
+        [Required(ErrorMessage = "Name Cannot be blank")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Prefix Cannot be blank")]
+        [StringLength(3)]
+        public string Prefix { get; set; }
+
+        [Display(Name = "Market Cap(in Billions)")]
+        public double MarketCap { get; set; }
+
+        [Required(ErrorMessage = "Price Cannot be blank")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "Date Cannot be blank")]
+        [Display(Name = "Date Created")]
+        public DateOnly DateCreated { get; set; }
+
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+
+
 
     }
 }
