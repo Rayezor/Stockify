d5b8382170974b87977b3061352a75711a9ce63e
Adam Geraghty <x00192288@mytudublin.ie>
2023-04-09 13:13:24 +0100

    Basic functionality of company complete

---
+++ Stockify/Controllers/CompanyController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stockify.Models;
+
+namespace Stockify.Controllers
+{
+    
+    public class CompanyController : Controller
+    {
+        private static List<Company> companylist = new List<Company>()
+        {
+
+            new Company
+            {
+                CompanyName= "Test Company",
+                StartDate = new DateOnly(2010, 1, 2),
+                Employees = 10,
+                Valuation = 12,
+                Market = "Crypto",
+                Industry = "Finance"
+            },
+        };
+
+
+
+        // GET: CompanyController
+        public ActionResult Company()
+        {
+            return View(companylist);
+        }
+
+        // GET: CompanyController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CompanyController/Create
+        public ActionResult CreateCompany()
+        {
+            return View();
+        }
+
+        // POST: CompanyController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateCompany(Company collection)
+        {
+            try
+            {
+
+
+                companylist.Add(collection);
+
+
+                return RedirectToAction("Company");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CompanyController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CompanyController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+     
+
+        // GET: CompanyController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CompanyController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
