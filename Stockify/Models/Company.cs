6887ce154a8cf62dabe9d5e388197dc2aa3d07c6
Adam Geraghty <x00192288@mytudublin.ie>
2023-04-09 13:00:57 +0100

    Updating company class

--- Stockify/Models/Company.cs
+++ Stockify/Models/Company.cs
@@ -1,6 +1,43 @@
-﻿namespace Stockify.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Stockify.Models
 {
     public class Company
     {
+
+        [Key]
+        [Required(ErrorMessage = "Company Cannot be blank")]
+        [Display(Name = "Company Name)")]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "Company Formation Date)")]
+        public DateOnly StartDate { get; set; }
+
+        [Display(Name = "Number of Employees)")]
+        public int Employees { get; set; }
+
+        [Display(Name = "Valuation (€))")]
+        public int Valuation { get; set; }
+
+        [Display(Name = "Stock Market)")]
+        public string Market { get; set; }
+
+        public string Industry { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
