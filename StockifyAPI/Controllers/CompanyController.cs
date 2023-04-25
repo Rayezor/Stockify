3c3de305ed07da5961f78ff27b5a22161270a946
Adam Geraghty <x00192288@mytudublin.ie>
2023-04-25 21:56:04 +0100

    Adding StockAPI RESTApi (needs db integration)

---
+++ StockifyAPI/Controllers/CompanyController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockifyAPI.Data;
+
+
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StockifyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public CompanyController(ApiContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/<CompanyController>
+        [HttpGet]
+        public JsonResult Get(string id)
+        {
+            var result = _context.Companies.Find(id);
+            if (result == null)
+                return new JsonResult(NotFound());
+            return new JsonResult(Ok(result));
+        }
+
+        // GET api/<CompanyController>/5
+        [HttpGet("/GetAll")]
+        public JsonResult GetAll()
+        {
+            var result = _context.Companies.ToList();
+            
+            return new JsonResult(Ok(result));
+        }
+
+    }
+}
---
+++ StockifyAPI/Data/ApiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockifyAPI.Models;
+
+namespace StockifyAPI.Data
+{
+    public class ApiContext:DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        { }
+    }
+}
